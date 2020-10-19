8cf63ad324b747557085db5ab3895c591ae714d3
Rithik Jain <rithik.jain3006@gmail.com>
2020-10-19 15:49:27 +0530

    🛠 Init project

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/rithikjain/local-businesses-backend
+
+go 1.14
+
+require (
+	github.com/andybalholm/brotli v1.0.1 // indirect
+    github.com/dgrijalva/jwt-go v3.2.0+incompatible
+    github.com/gofiber/fiber/v2 v2.0.6
+    github.com/gofiber/jwt/v2 v2.0.0
+    github.com/joho/godotenv v1.3.0
+    github.com/klauspost/compress v1.11.1 // indirect
+    golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+    golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211 // indirect
+    gorm.io/driver/postgres v1.0.2
+    gorm.io/gorm v1.20.2
+)
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+func dbConnect(host, port, user, dbname, password, sslmode string) (*gorm.DB, error) {
+	// In the case of heroku
+	if os.Getenv("DATABASE_URL") != "" {
+		return gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
+	}
+	db, err := gorm.Open(
+		postgres.Open(fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s", host, port, user, dbname, password, sslmode)),
+		&gorm.Config{},
+	)
+
+	return db, err
+}
+
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	if port == "" {
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to 4000")
+		return "localhost:3000"
+	}
+	return ":" + port
+}
+
+func main() {
+	if os.Getenv("onServer") != "True" {
+		// Loading the .env file
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+
+	// Setting up DB
+	_, err := dbConnect(
+		os.Getenv("dbHost"),
+		os.Getenv("dbPort"),
+		os.Getenv("dbUser"),
+		os.Getenv("dbName"),
+		os.Getenv("dbPass"),
+		os.Getenv("sslmode"),
+	)
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %s", err.Error())
+	}
+	fmt.Println("Connected to DB...")
+
+	app := fiber.New(fiber.Config{CaseSensitive: true})
+
+	app.Use(logger.New())
+
+	// Migrate Tables
+
+	// Make repos, services and  handlers
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hey there looks like its working 🔥")
+	})
+
+	fmt.Println("Serving...")
+	log.Fatal(app.Listen(GetPort()))
+}
